0f5a8266e9b0998bbaeead2be615f2254e8fcb09
Sergio Gonzalez <89554725+sergiogonzalezyes@users.noreply.github.com>
2023-12-26 00:50:58 -0600

    test

--- src/components/ScheduleLayout.vue
+++ src/components/ScheduleLayout.vue
@@ -203,6 +203,9 @@ export default {
     // Set the default focus to today's date in CST
     const today = new Date();
     today.setHours(today.getHours()); // Subtract 5 hours for CST (UTC-6)
+    console.log('FIRST Today:', today.setHours(today.getHours()))
+    console.log('SECOND Today:', today.setHours(today.getHours() - 6))
+
 
     // console.log('Today in CST:', today); // Add this line for debugging
     this.focus = today;
